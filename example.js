b7e92d4f63237deca2618352bac8b7a111511eed
Elizabeth Hale <lizzhale@mac.com>
2015-10-05 20:06:56 -0700

    Create a function that accepts a callback

--- example.js
+++ example.js
@@ -1,9 +1,14 @@
+function wait(seconds, callback) {
+    setTimeout(function() {
+        callback(new Date());
+    }, seconds * 1000);
+}
+
 console.log('Console log before calling setTimeout:', new Date());
 
-var seconds = 3;
-setTimeout(function() {
+wait(3, function() {
     console.log('Console log inside callbackFunction:', new Date());
-}, seconds * 1000);
+});
 
 
 console.log('Console log after calling setTimeout:', new Date());
