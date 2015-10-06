54cff4e053f3444437cf3dbdb7f417a63e1f54b7
Elizabeth Hale <lizzhale@mac.com>
2015-10-05 20:04:20 -0700

    Pass the callback function as an anonymous function. Notice that it affects the timing of the output

--- example.js
+++ example.js
@@ -1,10 +1,9 @@
 console.log('Console log before calling setTimeout:', new Date());
 
-var callbackFunction = function() {
-    console.log('Console log inside callbackFunction:', new Date());
-};
-
 var seconds = 3;
-setTimeout(callbackFunction, seconds * 1000);
+setTimeout(function() {
+    console.log('Console log inside callbackFunction:', new Date());
+}, seconds * 1000);
+
 
 console.log('Console log after calling setTimeout:', new Date());
